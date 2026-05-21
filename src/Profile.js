b98ae81fd7b09039e117b3a9e74f887b35d3d675
rekhabhoga43 <rekhabhoga43@gmail.com>
2026-05-21 17:19:48 +0530

    fixed vercel errors

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,7 @@ function Profile() {
             {data.map((iteam) => (
                 <div key={iteam.id}>
                     
-                    <img src={iteam.img} style={{width: "200px",height: "200px",borderRadius: "50%",objectFit: "cover",objectPosition:"top"}}/>
+                    <img src={iteam.img} alt="profile"style={{width: "200px",height: "200px",borderRadius: "50%",objectFit: "cover",objectPosition:"top"}}/>
                     <p> <b> username : </b> {iteam.username}</p>
                     <p> <b> email : </b> {iteam.email}</p>
                     <p> <b> subscribers: </b> {iteam.sub}</p>
